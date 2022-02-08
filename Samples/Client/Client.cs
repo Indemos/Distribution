15da1d3d28fe9fc1820d87c56eab77a3b1d5fdba
artemiusgreat <arteinvolo@gmail.com>
2022-02-08 12:51:53 -0500

    Add performance metrics to original code

--- Samples/Client/Client.cs
+++ Samples/Client/Client.cs
@@ -2,6 +2,7 @@ using Common;
 using Distribution.CommunicatorSpace;
 using Distribution.DomainSpace;
 using System;
+using System.Diagnostics;
 
 namespace ClientSpace
 {
@@ -23,7 +24,20 @@ namespace ClientSpace
     {
       var scene = new Scene();
       var message = new CreateMessage { Name = "Local Message" };
+
+      Stopwatch stopWatch = new Stopwatch();
+      stopWatch.Start();
+
       var response = scene.Send<DemoResponse>("Local Actor", message).Result;
+
+      stopWatch.Stop();
+
+      TimeSpan ts = stopWatch.Elapsed;
+
+      string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+          ts.Hours, ts.Minutes, ts.Seconds,
+          ts.Milliseconds / 10);
+      Console.WriteLine("RunTime " + elapsedTime);
 
       Console.WriteLine("Local Response : " + response.Data);
     }
